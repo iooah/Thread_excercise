13dea5c1f6e9efca4480e68ae7b6578c7ed43b0b
iooah <iooah@naver.com>
2024-09-10 13:42:26 +0900

    step1-2-counter-by-thread

--- src/main/java/com/nhnacademy/thread/CounterThread.java
+++ src/main/java/com/nhnacademy/thread/CounterThread.java
@@ -18,16 +18,21 @@ import java.util.Objects;
 
 @Slf4j
 //TODO#1 CounterThread는 Thread를 상속 합니다.
-public class CounterThread {
+public class CounterThread extends Thread {
+
     private final long countMaxSize;
 
     private long count;
 
     public CounterThread(String name, long countMaxSize) {
         //TODO#2 name <-- null 이거나 공백 문자열이면 IllegalArgumentException이 발생 합니다.
-
+        if (name.equals(null) || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         //TODO#3 countMaxSize <=0 이면 IllegalArgumentException이 발생 합니다.
-
+        if(countMaxSize <= 0) {
+            throw new IllegalArgumentException();
+        }
 
         this.setName(name);
         this.countMaxSize = countMaxSize;
@@ -35,7 +40,7 @@ public class CounterThread {
     }
 
     @Override
-    public void run() {
+    public void run(){
 
         /*TODO#4 run method를 구현 합니다.
             1초에 한 번식 다음과 같이 출력 됩니다.
@@ -46,6 +51,14 @@ public class CounterThread {
          */
 
         do {
+            try {
+                Thread.sleep(1000);
+                count++;
+                log.info("Thread: {} count: {}", this.getName(), count);
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
         }while (count<countMaxSize);
     }
