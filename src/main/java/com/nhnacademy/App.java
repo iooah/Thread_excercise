dd893515f5956d126da0f92348f4c11691c0b8a8
iooah <iooah@naver.com>
2024-09-10 14:11:46 +0900

    step1-5-counter-join-2

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -18,39 +18,41 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws InterruptedException {
         //TODO#1 counterHandlerA 객체를 생성 합니다. countMaxSize : 10
-        CounterHandler counterHandlerA;
+        CounterHandler counterHandlerA = new CounterHandler(10);
         //TODO#2 threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
-        Thread threadA;
+        Thread threadA  = new Thread(counterHandlerA);
         //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
-
+        threadA.setName("my-counter-A");
         log.debug("threadA-state:{}",threadA.getState());
 
 
         //TODO#4 counterHandlerB 객체를 생성 합니다. countMaxSize : 10
-        CounterHandler counterHandlerB;
+        CounterHandler counterHandlerB = new CounterHandler(10);
         //TODO#5 threadB 생성시 counterHandlerB 객체를 paramter로 전달 합니다.
-        Thread threadB;
+        Thread threadB = new Thread(counterHandlerB);
         //TODO#6 threadB의 name을 'my-counter-B' 로 설정 합니다.
-
+        threadB.setName("my-counter-B");
         log.debug("threadB-state:{}",threadB.getState());
 
         //TODO#7 threadA를 시작 합니다.
-
+        threadA.start();
 
         //TODO#8 threadA 작업이 완료될 때까지 main Thread는 대기 합니다.
+        threadA.join();
 
         log.debug("threadA-state:{}",threadA.getState());
 
         //TODO#9 threadB를 시작 합니다.
+        threadB.start();
 
         //TODO#10 threadB 작업이 완료될 때까지 main Thread는 대기 합니다.
+        threadB.join();
 
         log.debug("threadB-state:{}",threadB.getState());
 
         //TODO#11 'Application exit!' message를 출력 합니다.
-
+        log.debug("Application exit!");
     }
 }
